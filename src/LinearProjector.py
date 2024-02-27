a6029baeb5f4ece03faaa0f2515894bcffa8c4e0
Valentin De Matos <vltn@gmail.com>
2024-02-27 17:01:27 +0000

    doc: type hinting
    Signed-off-by: Valentin De Matos <vltn@gmail.com>

--- src/LinearProjector.py
+++ src/LinearProjector.py
@@ -2,14 +2,14 @@ from torch import nn, Tensor
 
 
 class LinearProjector(nn.Module):
-    def __init__(self, input_dim, output_dim, *args, **kwargs) -> None:
+    def __init__(self, input_dim: int, output_dim: int, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
 
-        self.linear_projector = nn.Sequential(
+        self._layers = nn.Sequential(
             nn.Linear(input_dim, 2048),
             nn.ReLU(),
             nn.Linear(2048, output_dim),
         )
 
-    def forward(self, t: Tensor) -> Tensor:
-        return self.linear_projector(t)
+    def forward(self, input_features: Tensor) -> Tensor:
+        return self._layers(input_features)
