d4baf46509a69b3c33b261d4ae2cc7e2c91999f5
Valentin De Matos <vltn@gmail.com>
2024-03-02 14:33:09 +0000

    fix: FrameDownSampler
    Signed-off-by: Valentin De Matos <vltn@gmail.com>

--- src/FramesDownSampler.py
+++ src/FramesDownSampler.py
@@ -6,23 +6,26 @@ from torch import nn, Tensor
 class FramesDownSampler(nn.Module):
     def __init__(self, k: int = 5) -> None:
         super().__init__()
-        
+
         self.k = k
-        
+
     def forward(self, frames: Tensor) -> Tensor:
+
+        frames = frames[::,:frames.size(1) - frames.size(1) % self.k]
+
         chunks = torch.chunk(frames, frames.size(1) // self.k, dim=1)
 
-        concatenated_frames = torch.cat(chunks, dim=0)
+        concatenated_frames = torch.stack(chunks).permute(1, 0, 2, 3)
 
-        down_sampled_frames = torch.sum(concatenated_frames, dim=1)
-        
-        return down_sampled_frames
+        downsampled_frames = torch.sum(concatenated_frames, dim=2)
+
+        return downsampled_frames
 
 
 if __name__ == "__main__":
     import torch
 
-    frames = torch.randn(8, 20)
-    
+    frames = torch.randn(1, 175, 1024)
+
     print("Input:", frames.shape)
     print("Output:", FramesDownSampler(k=5)(frames).shape)
