3e29ae8d3527c400d03c2b547e0ecb9957779309
Valentin De Matos <vltn@gmail.com>
2024-03-07 14:39:22 +0000

    fix(SLAM-ASR): imports & freeze encoder layers
    Signed-off-by: Valentin De Matos <vltn@gmail.com>

--- src/SLAM.py
+++ src/SLAM.py
@@ -1,10 +1,10 @@
 import torch
 
 from torch import nn, Tensor
-from .Encoder import Encoder
-from .Decoder import Decoder
-from .LinearProjector import LinearProjector
-from .FramesDownSampler import FramesDownSampler
+from Encoder import Encoder
+from Decoder import Decoder
+from LinearProjector import LinearProjector
+from FramesDownSampler import FramesDownSampler
 
 
 class SLAM(nn.Module):
@@ -20,19 +20,34 @@ class SLAM(nn.Module):
             output_dim=self.decoder.model.config.hidden_size,
         )
 
-    # TODO: it must accept batches!
-    # currently it takes as an input a single array representing a single audio sample
-    def forward(self, raw_speech: Tensor) -> Tensor:
+    def train(self, mode: bool = True):
+        super().train(mode)
 
-        speech_embeddings = self.encoder(raw_speech)
+        # Only train decoder
+        self.encoder.eval()
+
+        # Only train Linear layers
+        for name, param in self.decoder.named_parameters():
+            if not any([linear_indicator in name for linear_indicator in ('fc', 'dense', 'linear')]):
+                param.requires_grad = False
+
+        return self
+
+    def forward(
+        self,
+        input_values: Tensor,
+        labels: Tensor = None,
+        attention_mask: Tensor = None,
+        input_length: Tensor = None,
+    ) -> Tensor:
+
+        speech_embeddings = self.encoder(input_values)
 
         down_sampled_speech_embeddings = self.down_sampler(speech_embeddings)
 
         projected_speech_embeddings = self.linear_projector(down_sampled_speech_embeddings)
 
-        logits = self.decoder(speech_embeddings=projected_speech_embeddings)
-
-        return logits
+        return self.decoder(speech_embeddings=projected_speech_embeddings, labels=labels)
 
     def generate_transcript(self, raw_speech):
         logits = self.forward(raw_speech)
@@ -52,3 +67,13 @@ class SLAM(nn.Module):
 # SLAM generate_transcript method must be autoregressive to fully transcribe the input audio
 # Clean this file
 # Write README
+
+if __name__ == "__main__":
+    import torch
+
+    model = SLAM()
+    dummy_input = torch.randn((8, 258560))
+
+    output = model(dummy_input)
+
+    print(output['logits'].shape)
