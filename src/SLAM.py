12d78bc16bf03afa8dc17da277acf22de8e79911
Valentin De Matos <vltn@gmail.com>
2024-03-11 08:32:55 +0000

    feat: draft of generate_transcript method
    Signed-off-by: Valentin De Matos <vltn@gmail.com>

--- src/SLAM.py
+++ src/SLAM.py
@@ -3,8 +3,10 @@ import torch
 from torch import nn, Tensor
 from Encoder import Encoder
 from Decoder import Decoder
+from transformers import Wav2Vec2Processor
 from LinearProjector import LinearProjector
 from FramesDownSampler import FramesDownSampler
+from transformers.modeling_outputs import CausalLMOutputWithPast
 
 
 class SLAM(nn.Module):
@@ -14,11 +16,18 @@ class SLAM(nn.Module):
         self.encoder = Encoder(sampling_rate=16_000)
         self.down_sampler = FramesDownSampler(k=5)
         self.decoder = Decoder(model_name="microsoft/phi-2")
-
         self.linear_projector = LinearProjector(
             input_dim=self.encoder.output_dim,
             output_dim=self.decoder.model.config.hidden_size,
         )
+        self.processor = None
+
+    def _init_processor(self):
+        if self.processor is None:
+            self.processor = Wav2Vec2Processor(
+                feature_extractor=self.encoder.feature_extractor,
+                tokenizer=self.decoder.tokenizer,
+            )
 
     def train(self, mode: bool = True):
         super().train(mode)
@@ -32,6 +41,9 @@ class SLAM(nn.Module):
                 param.requires_grad = False
 
         return self
+
+    def eval(self):
+        return super().eval()
 
     def forward(
         self,
@@ -50,30 +62,56 @@ class SLAM(nn.Module):
         return self.decoder(speech_embeddings=projected_speech_embeddings, labels=labels)
 
     def generate_transcript(self, raw_speech):
-        logits = self.forward(raw_speech)
+        self._init_processor()
 
-        next_token_ids = torch.argmax(logits[:, -1, :], dim=-1)
+        input_values = raw_speech
+        # input_values = self.processor(
+        #     raw_speech,
+        #     sampling_rate=self.encoder.sampling_rate,
+        # ).input_values[0]
+
+        # input_features = [{"input_values": feature["input_values"]} for feature in features]
+
+        # input_values = self.processor.pad(
+        #     input_features,
+        #     padding=True,
+        #     # padding=self.padding_inputs,
+        #     # max_length=self.max_length_inputs,
+        #     return_tensors="pt",
+        # )["input_values"]
+
+        # print(input_values.shape)
+
+        # TODO: new func on decoder calling self.decoder.greedy_search
+        # https://github.com/huggingface/transformers/blob/v4.38.2/src/transformers/generation/utils.py#L1217
+
+        outputs: CausalLMOutputWithPast = self.forward(input_values)
+
+        next_token_ids = torch.argmax(outputs.logits[:, -1, :], dim=-1)
 
         new_tokens = self.decoder.tokenizer.convert_ids_to_tokens(next_token_ids.tolist())
+
+        print(new_tokens)
 
         # TODO: continue autoregressively
 
         return new_tokens[0], next_token_ids[0]
 
 # TODO LIST:
-# SLAM must takes a batched tensor as input and not a list representing a single audio sample
-# write data preprocessing
-# train the model using Trainer + link it to WnB
 # SLAM generate_transcript method must be autoregressive to fully transcribe the input audio
-# Clean this file
 # Write README
+
 
 if __name__ == "__main__":
     import torch
 
     model = SLAM()
-    dummy_input = torch.randn((8, 258560))
+    dummy_input_values = torch.randn((8, 258560))
 
-    output = model(dummy_input)
+    output = model(dummy_input_values)
+
+    print("Output:", output['logits'].shape)
+
+    output = model.generate_transcript(dummy_input_values)
 
     print(output['logits'].shape)
