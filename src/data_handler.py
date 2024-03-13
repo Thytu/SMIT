67e477d1ce5a4ae6facd9b3919980e16e6d165b7
Valentin De Matos <vltn@gmail.com>
2024-03-13 19:53:59 +0000

    feat: draft multi-task training
    Signed-off-by: Valentin De Matos <vltn@gmail.com>

--- src/data_handler.py
+++ src/data_handler.py
@@ -1,38 +1,16 @@
 import torch
 
 from typing import Dict
+from SLAM import SLAMInput
 from transformers import Wav2Vec2Processor
-from datasets import Dataset, load_dataset, Audio
-
+from datasets import Dataset, load_dataset, Audio, concatenate_datasets
 
 LIBRISPEECH_ASR = "librispeech_asr"
 SUBSET = "all"
 SAMPLING_RATE = 16_000
 
 
-def downsample_frames(frames: torch.Tensor, k: int = 5):
-    """Down sample frames by concatenating every k consecutive frames in the feature dimension.
-
-    Args:
-        frames (torch.Tensor): frames to concatenate, usually encoder ouput
-        k (int, optional): Downsampling rate. Defaults to 5.
-
-    Returns:
-        torch.Tensor: concatenated frames
-    """
-
-    frames = frames[::,:frames.size(1) - frames.size(1) % k]
-
-    chunks = torch.chunk(frames, frames.size(1) // k, dim=1)
-
-    concatenated_frames = torch.stack(chunks).permute(1, 0, 2, 3)
-
-    downsampled_frames = torch.sum(concatenated_frames, dim=2)
-
-    return downsampled_frames
-
-
-def load_processed_dataset(
+def load_processed_librispeech_dataset(
     train_split_size: str = None,
     test_split_size: str = None,
     validation_split_size: str = None,
@@ -61,15 +39,33 @@ def load_processed_dataset(
     }
 
 
-def add_raw_speech_feature_to_dataset(batch, processor: Wav2Vec2Processor):
-    value = processor(
+def load_processed_instruct_dataset(
+    test_size: float,
+) -> Dict[str, Dataset]:
+
+    instruct_dataset = load_dataset(
+        "WizardLM/WizardLM_evol_instruct_70k",
+        split="train",
+    ).train_test_split(test_size=test_size)
+
+    return {
+        "train": instruct_dataset["train"].shuffle(),
+        "test": instruct_dataset["test"].shuffle(),
+    }
+
+
+def add_raw_speech_feature_to_librispeech_dataset(batch, processor: Wav2Vec2Processor):
+
+    resampled_audio = processor(
         batch["audio"]["array"],
         sampling_rate=batch["audio"]["sampling_rate"]
     ).input_values[0]
 
-    batch["input_values"] = value
-
-    batch["input_length"] = len(batch["input_values"])
+    batch["inputs"] = {
+        "instruct": "Transcribe speech to text {audio}",
+        "raw_audio": resampled_audio,
+    }
+    batch["input_length"] = len(batch["inputs"])
 
     batch["labels"] = processor(
         text=batch["text"].capitalize() + ".",
@@ -81,6 +77,24 @@ def add_raw_speech_feature_to_dataset(batch, processor: Wav2Vec2Processor):
     return batch
 
 
+def process_instruct_dataset(batch):
+
+    batch["inputs"] = {
+        "instruct": batch["instruction"],
+        "raw_audio": None,
+    }
+    batch["input_length"] = len(batch["inputs"])
+
+    batch["labels"] = processor(text=batch["output"]).input_ids
+
+    # TODO: Verify if I should add the EOS token
+    if len(batch["labels"]) > 0 and batch["labels"][-1] != processor.tokenizer.eos_token_id:
+        batch["labels"].append(processor.tokenizer.eos_token_id)
+
+    return batch
+
+
+
 if __name__ == "__main__":
 
     import os
@@ -90,8 +104,16 @@ if __name__ == "__main__":
     from Decoder import Decoder
     from datasets import DatasetDict
 
+    MAX_LENGTH = 2048
 
-    _dataset = load_processed_dataset()
+    _dataset = load_processed_librispeech_dataset(
+        # train_split_size="[:100]",
+        # test_split_size="[:100]",
+        # validation_split_size="[:100]",
+    )
+
+    instruct_dataset = load_processed_instruct_dataset(test_size=0.2)
+    instruct_dataset["validation"] = instruct_dataset["test"]
 
     rand_int = random.randint(0, len(_dataset['train'])-1)
 
@@ -107,12 +129,34 @@ if __name__ == "__main__":
     os.makedirs("outputs/dataset/", exist_ok=True)
 
     for set_name in ("train", "test", "validation"):
+
+        # _dataset[set_name] = _dataset[set_name][:min(len(instruct_dataset[set_name]), len(_dataset[set_name]))]
+        # instruct_dataset[set_name] = instruct_dataset[set_name][:min(len(instruct_dataset[set_name]), len(_dataset[set_name]))]
+
+        nb_samples_from_each_dataset = min(len(instruct_dataset[set_name]), len(_dataset[set_name]))
+        instruct_dataset[set_name] = instruct_dataset[set_name].select([i for i in range(nb_samples_from_each_dataset)])
+
+        instruct_dataset[set_name] = instruct_dataset[set_name].map(
+            process_instruct_dataset,
+            remove_columns=instruct_dataset[set_name].column_names,
+            num_proc=max(1, os.cpu_count() - 1),
+        ).filter(lambda sample: len(sample["labels"]) > 0 and len(sample["inputs"]["instruct"]) + len(sample["labels"]) > MAX_LENGTH)
+
+        # Update after filtering instruct dataset
+        nb_samples_from_each_dataset = min(len(instruct_dataset[set_name]), len(_dataset[set_name]))
+        _dataset[set_name] = _dataset[set_name].select([i for i in range(nb_samples_from_each_dataset)])
+
         _dataset[set_name] = _dataset[set_name].map(
-            add_raw_speech_feature_to_dataset,
+            add_raw_speech_feature_to_librispeech_dataset,
             remove_columns=_dataset[set_name].column_names,
             fn_kwargs={"processor": processor},
             num_proc=max(1, os.cpu_count() - 1),
         )
+
+        _dataset[set_name] = concatenate_datasets([
+            _dataset[set_name],
+            instruct_dataset[set_name]
+        ]).shuffle()
 
     DatasetDict(_dataset).save_to_disk(
         dataset_dict_path=f"outputs/dataset/",
