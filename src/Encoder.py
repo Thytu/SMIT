baa39b3ae1ea91c5fc431c13284bd8e6712cf37f
Valentin De Matos <vltn@gmail.com>
2024-03-18 15:37:15 +0000

    refactor(Encoder): expects model_name as input
    Signed-off-by: Valentin De Matos <vltn@gmail.com>

--- src/Encoder.py
+++ src/Encoder.py
@@ -3,16 +3,22 @@ from transformers import HubertModel, Wav2Vec2FeatureExtractor
 
 
 class Encoder(nn.Module):
-
-
-    def __init__(self, sampling_rate, *args, **kwargs) -> None:
+    def __init__(
+            self,
+            model_name: str,
+            sampling_rate,
+            feature_extractor: str = None,
+            *args, **kwargs
+        ) -> None:
         super().__init__(*args, **kwargs)
 
         self.sampling_rate = sampling_rate
 
         # TODO: find a way to not use the ft version
-        self.model = HubertModel.from_pretrained("facebook/hubert-large-ls960-ft")
-        self.feature_extractor = Wav2Vec2FeatureExtractor.from_pretrained("facebook/hubert-base-ls960")
+        self.model = HubertModel.from_pretrained(model_name)
+        self.feature_extractor = Wav2Vec2FeatureExtractor.from_pretrained(
+            feature_extractor if feature_extractor is not None else model_name
+        )
         self.output_dim = self.model.config.hidden_size
 
     # TODO: add call to self.feature_extractor when using SLAM.generate_transcript
@@ -24,7 +30,12 @@ if __name__ == "__main__":
     import torch
 
     dummy_input = torch.randn((8, 258560))
-    encoder = Encoder(sampling_rate=16_000)
+
+    encoder = Encoder(
+        model_name="facebook/hubert-large-ls960-ft",
+        feature_extractor="facebook/hubert-base-ls960",
+        sampling_rate=16_000,
+    )
 
     output = encoder(raw_speech=dummy_input)
 
