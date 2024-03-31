e2f869670ddce1775fc3e28473f08fef12d47947
Valentin De Matos <vltn.dematos@gmail.com>
2024-03-31 14:11:07 +0000

    refactor(fn_factory): export _access_value fn
    Signed-off-by: Valentin De Matos <vltn.dematos@gmail.com>

--- src/fn_factory.py
+++ src/fn_factory.py
@@ -4,7 +4,7 @@ import functools
 from typing import List
 
 
-def __access_value(dict_to_access, key_as_str):
+def _access_value(dict_to_access, key_as_str):
     if "." in key_as_str:
         return functools.reduce(lambda d, k: d[k], key_as_str.split('.'), dict_to_access)
 
@@ -16,7 +16,7 @@ def is_shorter_than_model_max_length(columns: List[str], dataset, tokenizer, saf
 
     for column in columns:
         dataset = dataset.filter(
-            lambda sample: len(__access_value(sample, column)) < model_max_length - safe_padding,
+            lambda sample: len(_access_value(sample, column)) < model_max_length - safe_padding,
             num_proc=max(1, os.cpu_count() - 1),
         )
 
@@ -27,7 +27,7 @@ def is_longer_than(columns: List[str], value, dataset, **kwargs):
 
     for column in columns:
         dataset = dataset.filter(
-            lambda sample: len(__access_value(sample, column)) > value,
+            lambda sample: len(_access_value(sample, column)) > value,
             num_proc=max(1, os.cpu_count() - 1),
         )
 
